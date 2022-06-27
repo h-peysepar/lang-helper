996c9b59eb0db603437476df15e6c8cb8a170b36
HadiPr <hadipeysepar79@gmail.com>
2022-06-27 20:13:18 +0430

    prevent form re-rendering

--- src/pages/add-word/index.tsx
+++ src/pages/add-word/index.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { ChangeEvent, useEffect, useMemo } from 'react';
+import { useForm, useWatch } from 'react-hook-form';
 import { useMutation } from 'react-query';
 import Button from '../../components/Button';
 import ErrorMessage from '../../components/errorMessage';
@@ -15,17 +15,55 @@ export default function AddForm(props: AddFormProps) {
     formState: { errors },
     register,
     setValue,
+    control,
+    reset,
   } = useForm();
 
-  const { mutate, isLoading: storeWordLoading } = useMutation(data => axios.post('/words', data));
-  const { mutate: getDefaultDefinition, data: definition, isLoading } = useMutation(word =>
-    axios.get('/default-definition/' + word)
-  );
+  const {
+    mutate,
+    isLoading: storeWordLoading,
+    data,
+  } = useMutation(data => axios.post('/words', data));
 
   useEffect(() => {
-    setValue('defaultDefinition', definition?.data?.data);
-  }, [definition]);
+    data && reset();
+  }, [data]);
+
   const onSubmit = (data: object) => mutate(data);
+
+  const components = useMemo(
+    () => ({
+      Definition({}) {
+        const word = useWatch({ control, name: 'word' });
+        const {
+          mutate: getDefaultDefinition,
+          data: definition,
+          isLoading,
+        } = useMutation(word => axios.get('/default-definition/' + word));
+        useEffect(() => {
+          word ? getDefaultDefinition(word) : setValue('defaultDefinition', '');
+        }, [word]);
+        useEffect(() => {
+          setValue('defaultDefinition', definition?.data?.data || '');
+        }, [definition]);
+        return (
+          <div className='mb-4'>
+            <Label>Translate</Label>
+            <span className='relative'>
+              {isLoading && <Loading />}
+              <Input
+                {...register('defaultDefinition')}
+                disabled
+                className='general-input'
+              />
+            </span>
+            <ErrorMessage>{''}</ErrorMessage>
+          </div>
+        );
+      },
+    }),
+    []
+  );
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='p-3'>
@@ -33,36 +71,13 @@ export default function AddForm(props: AddFormProps) {
         <Label>Word</Label>
         <Input
           className='general-input'
-          {...(function () {
-            const { onChange, ...methods } = register('word', {
-              required: 'Please Enter The Word',
-            });
-            return {
-              onChange: (e: any) => {
-                if (e.target.value.trim()) getDefaultDefinition(e.target.value)
-                else {
-                  setValue('defaultDefinition', '')
-                }
-                onChange(e);
-              },
-              ...methods,
-            };
-          })()}
+          {...register('word', {
+            required: 'Please Enter The Word',
+          })}
         />
         <ErrorMessage>{errors.word?.message}</ErrorMessage>
       </div>
-      <div className='mb-4'>
-        <Label>Translate</Label>
-        <span className='relative'>
-          {isLoading && <Loading />}
-          <Input
-            {...register('defaultDefinition')}
-            disabled
-            className='general-input'
-          />
-        </span>
-        <ErrorMessage>{''}</ErrorMessage>
-      </div>
+      <components.Definition />
       <div className='mb-4'>
         <Label>Custom Definition</Label>
         <Input {...register('definition')} className='general-input' />
